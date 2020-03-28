241b081e2859cf5a03685217cb07147d9f247500
Frank Flores <frankthetank67@gmail.com>
2020-03-28 09:57:20 -0700

    created an snapshot value variable to reference the current child in the database to display in my html, added moment.js cdn to my html and used moment to convert the inputted time by user to military time, and made variable to get the current time, to get the difference from the current time to my converted time. and variables to get the time of the next train will be coming in minutes

--- js/app.js
+++ js/app.js
@@ -1,49 +1,43 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyA_BO_r3Fpmr97G51dWWlZza9a3bAgH9qY",
-    authDomain: "train-schedule-e0006.firebaseapp.com",
-    databaseURL: "https://train-schedule-e0006.firebaseio.com",
-    projectId: "train-schedule-e0006",
-    storageBucket: "train-schedule-e0006.appspot.com",
-    messagingSenderId: "663107929281",
-    appId: "1:663107929281:web:1f50b1c3b118f28089c040",
-    measurementId: "G-YJ196LVQ4V"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA_BO_r3Fpmr97G51dWWlZza9a3bAgH9qY",
+  authDomain: "train-schedule-e0006.firebaseapp.com",
+  databaseURL: "https://train-schedule-e0006.firebaseio.com",
+  projectId: "train-schedule-e0006",
+  storageBucket: "train-schedule-e0006.appspot.com",
+  messagingSenderId: "663107929281",
+  appId: "1:663107929281:web:1f50b1c3b118f28089c040",
+  measurementId: "G-YJ196LVQ4V"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var name, dest, time, freq;
-  var arrival = "";
-  var minAway = 0;
+var name, dest, firstTime, tFreq;
 
+$("#submit-train").on("click", function(event) {
+  event.preventDefault();
 
-      $("#submit-train").on("click", function(event) {
-        event.preventDefault();
+  name = $("#name-input").val();
+  dest = $("#destination-input").val();
+  firstTime = $("#train-time-input").val();
+  tFreq = $("#frequency-input").val();
 
-        name = $("#name-input").val();
-        dest = $("#destination-input").val();
-        time = $("#train-time-input").val();
-        freq = $("#frequency-input").val();
-
-
-
-        console.log(name, dest, time, freq);
-
-        database.ref().push({
-            train_name: name,
-      destination: dest,
-      time: time,
-      frequency: freq,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-        });
-      });
+  database.ref().push({
+    train_name: name,
+    destination: dest,
+    time: firstTime,
+    frequency: tFreq,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+});
 
 // Firebase watcher .on("child_added"
-database.ref().on("child_added", function(snapshot) {
+database.ref().on(
+  "child_added",
+  function(snapshot) {
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
 
@@ -53,13 +47,30 @@ database.ref().on("child_added", function(snapshot) {
     console.log(sv.time);
     console.log(sv.frequency);
 
-    
+    firstTimeConverted = moment(sv.time, "HH:mm a").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-    $("tbody").append(`<tr><td scope="row">${name}</td><td>${sv.destination}</td><td>${sv.frequency}</td><td>${arrival}</td><td>${minAway}</td></tr>`);
+    var currentTime = moment();
 
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    var tRemainder = diffTime % sv.frequency;
+
+    var tMinutesTillTrain = sv.frequency - tRemainder;
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+    $("tbody").append(
+      `<tr><td scope="row">${sv.train_name}</td><td>${sv.destination}</td><td>${
+        sv.frequency
+      }</td><td>${moment(nextTrain).format(
+        "hh:mm a"
+      )}</td><td>${tMinutesTillTrain}</td></tr>`
+    );
 
     // Handle the errors
-  }, function(errorObject) {
+  },
+  function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
-  });
-
+  }
+);
